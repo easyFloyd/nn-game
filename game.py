df327ab6ff9f6b0ddd310d047656315942f8afd9
easyFloyd <attila.kiraly.28@gmail.com>
2018-01-19 17:11:48 +0100

    Update game.py
    hard option

--- game.py
+++ game.py
@@ -1,11 +1,18 @@
 from random import randint
 
 class Game:
-	def __init__(self, board_width = 20, board_height = 20, opponents_number = 20, gui = False):
+	def __init__(self, board_width = 20, board_height = 20, opponents_number = 3, gui = False, hard = False):
 		self.gui = gui
+		self.hard = hard
 		self.opponents_number = opponents_number
 		self.board = {'width': board_width, 'height': board_height}
 		self.result = -1 #-1: ingame, 0: lost, 1: win
+		self.vectors_and_keys = [
+			[[0, 1], 0],
+			[[0, -1], 1],
+			[[-1, 0], 2],
+			[[1, 0], 3]
+			]
 		
 	def start(self):
 		self.stepcount = 0
@@ -43,10 +50,10 @@ class Game:
 		# 1 - DOWN
 		# 2 - LEFT
 		# 3 - RIGHT
-		if self.result != -1: self.end_game()
 		self.stepcount += 1
 		self.move(self.player, direction)
 		self.check_result()
+		if self.result != -1: self.end_game()
 		for opponent in self.opponents:
 			self.move_opponent(opponent)
 		return self.get_status()
@@ -76,24 +83,90 @@ class Game:
 			self.result = 1
 	
 	def move_opponent(self, opponent):
-		orig = list(opponent)
-		self.move(opponent, randint(0,3))
-		if (opponent == self.player or
-			opponent[0] == 0 or
-			opponent[0] == self.board["width"] + 1 or
-			opponent[1] == 0 or
-			opponent[1] == self.board["height"] + 1):
-			opponent[0] = orig[0]
-			opponent[1] = orig[1]
+		direction = self.get_opponent_direction(opponent)
+		if direction != -1: self.move(opponent, direction)
+		
+	def get_opponent_direction(self, opponent):
+		if self.hard:
+			horizontal, vertical = self.get_opponent_move_vector_hard(opponent)
+		else:
+			horizontal, vertical = self.get_opponent_move_vector_easy()
+		
+		if horizontal == 1 and self.is_valid_opponent_move([0,horizontal], opponent):
+			return self.get_direction_by_vector([0,horizontal])
+		if randint(0,1) == 0:
+			if vertical != 0 and self.is_valid_opponent_move([vertical,0], opponent):
+				return self.get_direction_by_vector([vertical,0])
+			if horizontal == -1 and self.is_valid_opponent_move([0,horizontal], opponent):
+				return self.get_direction_by_vector([0,horizontal])
+		else:
+			if horizontal == -1 and self.is_valid_opponent_move([0,horizontal], opponent):
+				return self.get_direction_by_vector([0,horizontal])
+			if vertical != 0 and self.is_valid_opponent_move([vertical,0], opponent):
+				return self.get_direction_by_vector([vertical,0])
+		return -1
+		
+	def get_opponent_move_vector_hard(self, opponent):
+		orig_point = [opponent[0], opponent[1]]
+		target = [self.player[0], self.player[1] + 1]
+		vertical = 0 # -1: Right, +1: Left
+		horizontal = 0 # -1: Down, +1: Up
+		if orig_point[0] < target[0]:
+			vertical = 1
+		elif orig_point[0] > target[0]:
+			vertical = -1
+		if orig_point[1] < target[1]:
+			horizontal = 1
+		elif orig_point[1] > target[1]:
+			horizontal = -1
+		return horizontal, vertical
+		
+	def get_opponent_move_vector_easy(self):
+		return randint(-1,1), randint(-1,1)
+		
+		
+	def is_valid_opponent_move(self, vector, orig_point):
+		result = [orig_point[0] + vector[0], orig_point[1] + vector[1]]
+		if (result == self.player or
+			result in self.opponents or
+			result[0] == 0 or
+			result[0] == self.board["width"] + 1 or
+			result[1] == 0 or
+			result[1] == self.board["height"] + 1):
+			return False
+		else:
+			return True
+	
+	def get_direction_by_vector(self, vector):
+		for v in self.vectors_and_keys:
+			if v[0] == vector: return v[1]
 	
 	def end_game(self):
-		raise Exception("Game over")
+		msg = ""
+		if self.result == 1:
+			msg = "GOAL"
+		else:
+			msg = "FAIL"
+		raise Exception(msg)
 	
 	def get_status(self):
 		return self.result, self.stepcount, self.player, self.opponents, self.goal
 	
 if __name__ == "__main__":
-	game = Game()
-	print(game.start())
-	for _ in range(20):
-		print(game.step(randint(0,3)))
+	game = Game(hard = True)
+	result, stepcount, player, opponents, goal = game.start()
+	print("----------------")
+	print("START")
+	print(" - Player: ", player)
+	print(" - Opponents: ", opponents)
+	print(" - Goal: ", goal)
+	for _ in range(25):
+		direction = 0 #randint(0,3)
+		print("----------------")
+		print("Next move: ", direction)
+		result, stepcount, player, opponents, _ = game.step(direction)
+		print("Step: ", stepcount)
+		print(" - Result: ", result)
+		print(" - Player: ", player)
+		print(" - Opponents: ", opponents)
+	
